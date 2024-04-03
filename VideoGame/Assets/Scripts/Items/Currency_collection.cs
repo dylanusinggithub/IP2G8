0fdf5b434df915edc462cf000d5270c28e2bf2fc
jacklawrence <122370806+Jack-Lawrence@users.noreply.github.com>
2024-04-04 00:29:08 +0100

    Sorting Layer Fix

--- VideoGame/Assets/Scripts/Items/Currency_collection.cs
+++ VideoGame/Assets/Scripts/Items/Currency_collection.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class Currency_collection : MonoBehaviour
 {
     private GameManager gameManager;
+    private Transform playerTransform;
+    public float moveSpeed = 2f;
 
     void Start()
     {
         gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, playerTransform.position) <= 5f)
+        {
+            transform.position = Vector2.Lerp(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
