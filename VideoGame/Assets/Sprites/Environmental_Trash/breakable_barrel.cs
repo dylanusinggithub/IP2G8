090d656fef89affe91e25d66db5274e34617f418
Jack Lawrence <jacklawrence27@icloud.com>
2024-04-18 09:18:04 +0100

    Pots + Barrels Update
    They now have a chance to drop Gems / Half hearts
    
    The player also starts with 1 more heart
    
    And the boss has gone from 500 -> 300hp

--- VideoGame/Assets/Sprites/Environmental_Trash/breakable_barrel.cs
+++ VideoGame/Assets/Sprites/Environmental_Trash/breakable_barrel.cs
@@ -7,14 +7,16 @@ public class breakable_barrel : MonoBehaviour
     private Animator anim;
     private AudioManager audioManager;
 
+    [Header("Enemy Drops")]
+    public Sprite[] barrelDrops;
+    private GameObject spawnLocation;
+    public float barrelDropChance = 30f;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         audioManager = FindFirstObjectByType<AudioManager>();
-    }
-    void Update()
-    {
-
+        spawnLocation = this.gameObject;
     }
 
     public void Smash()
@@ -27,6 +29,53 @@ public class breakable_barrel : MonoBehaviour
     IEnumerator breakCo()
     {
         yield return new WaitForSeconds(.3f);
-        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
+        DropChance();
+    }
+
+    public void DropChance()
+    {
+        float dropChance = barrelDropChance;
+
+        if (Random.value * 100 <= dropChance)
+        {
+            SpawnDrop(barrelDrops);
+        }
+    }
+
+
+    void SpawnDrop(Sprite[] itemsArray)
+    {
+        if (itemsArray != null && itemsArray.Length > 0)
+        {
+            UnityEngine.Random.InitState(System.DateTime.Now.Millisecond);
+            int randomIndex = Random.Range(0, itemsArray.Length);
+            Debug.Log("Random Index: " + randomIndex);
+            Debug.Log("Array Length: " + itemsArray.Length);
+
+            Sprite chosenItem = itemsArray[randomIndex];
+
+            //Create the item above the pedestal
+            GameObject newItem = new GameObject("EnemyDrop");
+
+            newItem.transform.position = spawnLocation.transform.position;
+
+            //Add a sprite renderer with values
+            SpriteRenderer spriteRenderer = newItem.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = chosenItem;
+            spriteRenderer.sortingOrder = 2;
+
+            //Change the scale of the sprite / My references are too small can be removed later on
+            float scaleMultiplier = 1.25f;
+            newItem.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1.0f);
+
+            //Attach item script to the object and run internal function
+            EnemyItem item = newItem.AddComponent<EnemyItem>();
+            item.InitializeItem(chosenItem, itemsArray);
+
+            //Attach 2D collider with trigger so it can be interacted with
+            BoxCollider2D collider = newItem.AddComponent<BoxCollider2D>();
+            collider.isTrigger = true;
+        }
     }
 }
--- VideoGame/Assets/Sprites/Environmental_Trash/breakable_pot.cs
+++ VideoGame/Assets/Sprites/Environmental_Trash/breakable_pot.cs
@@ -9,10 +9,16 @@ public class breakable_pot : MonoBehaviour
     private Animator anim;
     private AudioManager audioManager;
 
+    [Header("Enemy Drops")]
+    public Sprite[] potDrops;
+    private GameObject spawnLocation;
+    public float potDropChance = 30f;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         audioManager = FindFirstObjectByType<AudioManager>();
+        spawnLocation = this.gameObject;
     }
     void Update()
     {
@@ -29,6 +35,53 @@ public class breakable_pot : MonoBehaviour
     IEnumerator breakCo()
     {
         yield return new WaitForSeconds(.3f);
-        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
+        DropChance();
+    }
+
+    public void DropChance()
+    {
+        float dropChance = potDropChance;
+
+        if (Random.value * 100 <= dropChance)
+        {
+            SpawnDrop(potDrops);
+        }
+    }
+
+
+    void SpawnDrop(Sprite[] itemsArray)
+    {
+        if (itemsArray != null && itemsArray.Length > 0)
+        {
+            UnityEngine.Random.InitState(System.DateTime.Now.Millisecond);
+            int randomIndex = Random.Range(0, itemsArray.Length);
+            Debug.Log("Random Index: " + randomIndex);
+            Debug.Log("Array Length: " + itemsArray.Length);
+
+            Sprite chosenItem = itemsArray[randomIndex];
+
+            //Create the item above the pedestal
+            GameObject newItem = new GameObject("EnemyDrop");
+
+            newItem.transform.position = spawnLocation.transform.position;
+
+            //Add a sprite renderer with values
+            SpriteRenderer spriteRenderer = newItem.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = chosenItem;
+            spriteRenderer.sortingOrder = 2;
+
+            //Change the scale of the sprite / My references are too small can be removed later on
+            float scaleMultiplier = 1.25f;
+            newItem.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1.0f);
+
+            //Attach item script to the object and run internal function
+            EnemyItem item = newItem.AddComponent<EnemyItem>();
+            item.InitializeItem(chosenItem, itemsArray);
+
+            //Attach 2D collider with trigger so it can be interacted with
+            BoxCollider2D collider = newItem.AddComponent<BoxCollider2D>();
+            collider.isTrigger = true;
+        }
     }
 }
